a501d8513966bcac3529d09159452f80b9c743b7
vdlindenmark <vdlindenmark@gmail.com>
2016-04-13 23:06:33 +0200

    Tijds controller aangepast

--- app/controllers/tijds_controller.rb
+++ app/controllers/tijds_controller.rb
@@ -1,2 +1,20 @@
 class TijdsController < ApplicationController
+
+	def create
+	  	@tijd = Tijd.new(tijd_params)
+	  	if @tijd.save
+	  		redirect_to tijds_path
+	  	end
+	end
+
+	def index
+	end
+
+	def show
+	end
+
+	def new
+		@tijd = Tijd.new()
+	end
+
 end
