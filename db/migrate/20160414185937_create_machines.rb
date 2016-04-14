abb4b93e5c48e6056db01c82ecc9d5ee4c25663a
RuerdVisser1991 <ruerdvisser@live.nl>
2016-04-14 21:01:24 +0200

    Migration aangepast van type naar kind

--- db/migrate/20160414185937_create_machines.rb
+++ db/migrate/20160414185937_create_machines.rb
@@ -1,10 +1,8 @@
 class CreateMachines < ActiveRecord::Migration
   def change
     create_table :machines do |t|
-      t.integer :type
+      t.integer :kind
       t.integer :machine_id
-
-      t.timestamps null: false
     end
   end
 end
