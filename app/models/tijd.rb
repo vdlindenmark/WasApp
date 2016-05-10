9fa73491a332641f1206d7382091dbc1e44e607a
vdlindenmark <vdlindenmark@gmail.com>
2016-05-10 18:24:26 +0200

    cronjob aangemaakt om te kijken of er al wasbeurten klaar zijn. Deze worden nu alleen nog in cron.log geplaatst.

--- app/models/tijd.rb
+++ app/models/tijd.rb
@@ -1,3 +1,14 @@
 class Tijd < ActiveRecord::Base
 	belongs_to :machine
+
+	def self.delete_run_innings
+		@tijd_nu = Time.now
+		@tijd_nu = @tijd_nu.strftime("%d/%m/%Y %H:%M")
+		tijds = Tijd.where('end_datetime < ?', @tijd_nu)
+
+		tijds.each do |tijd|
+			#tijd.destroy
+			puts tijd.end_datetime
+		end
+	end
 end
